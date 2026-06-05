51880eee8fb25ace10c418cedfb7be8cdd409901
Sujai <sujaiit0696@gmail.com>
2026-06-05 20:05:58 +0530

    fix: replace Jinja include bundle files with plain JS/CSS for esbuild compatibility

--- construction_marketplace/public/js/construction_marketplace.bundle.js
+++ construction_marketplace/public/js/construction_marketplace.bundle.js
@@ -2,4 +2,33 @@
 // For license information, please see license.txt
 
 // Construction Marketplace JS Bundle
-{% include "construction_marketplace/public/js/construction_marketplace.js" %}
+
+frappe.provide("construction_marketplace");
+
+frappe.pages["construction-marketplace-dashboard"].onload = function(wrapper) {
+    frappe.call({
+        method: "construction_marketplace.construction_marketplace.utils.get_dashboard_data",
+        callback: function(r) {
+            if (r.message) {
+                // Render dashboard with data
+            }
+        }
+    });
+};
+
+frappe.form.formatters["Material Category"] = function(value, doc, fieldname) {
+    if (value) {
+        return `<span class="cm-category-badge">${value}</span>`;
+    }
+    return value;
+};
+
+$(document).on("form-render", function(e, wrapper) {
+    if (wrapper.data && wrapper.data.doctype === "Marketplace Order") {
+        var status = wrapper.data.status;
+        if (status) {
+            wrapper.$wrapper.find(".page-title")
+                .addClass("order-status-" + status.toLowerCase());
+        }
+    }
+});
