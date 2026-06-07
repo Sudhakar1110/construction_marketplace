52c99be97432126a1144b46ee656d485da498028
Sujai <sujaiit0696@gmail.com>
2026-06-07 20:38:14 +0530

    fix: move child table doctypes to top-level directory so Frappe can find their controllers

---
+++ construction_marketplace/construction_marketplace/doctype/material_specification/material_specification.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2024, Sudhakar and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+
+class MaterialSpecification(Document):
+    """Material Specification child table for Construction Material"""
+    pass
